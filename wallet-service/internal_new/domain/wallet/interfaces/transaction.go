68798934f591ab0fb2efe85fb9d520a55f5ed1a5
Billy <moryakovpirat@gmail.com>
2025-09-25 22:14:43 +0300

    -

--- wallet-service/internal_new/domain/wallet/interfaces/transaction.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-import "context"
-
-type TransactionWallet interface {
-	ReserveFunds(ctx context.Context, userID int64, amount float64) error
-	ReleaseFunds(ctx context.Context, userID int64, amount float64) error
-	CommitFunds(ctx context.Context, userID int64, amount float64) error
-}
--- wallet-service/internal_new/domain/wallet/interfaces/user.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "context"
-
-type UserWallet interface {
-	GetBalance(ctx context.Context, userID int64) (float64, error)
-	UpdateBalance(ctx context.Context, userID int64, amount float64) error
-}
